133699f7df636e2059d041855e8180d14a6a63d8
Cuau95 <elzua_ecm@live.com.mc>
2019-05-22 22:23:19 -0500

    fix password response

---
+++ src/main/java/com/titulation/company/configuration/domain/model/PasswordResponse.java
@@ -0,0 +1,17 @@
+package com.titulation.company.configuration.domain.model;
+
+import java.io.Serializable;
+
+public class PasswordResponse implements Serializable {
+    
+    private String passwordResponse;
+
+    public String getPasswordResponse() {
+        return passwordResponse;
+    }
+
+    public void setPasswordResponse(String passwordResponse) {
+        this.passwordResponse = passwordResponse;
+    }
+    
+}
--- src/main/java/com/titulation/company/configuration/infratructure/controller/CompanyContoller.java
+++ src/main/java/com/titulation/company/configuration/infratructure/controller/CompanyContoller.java
@@ -5,11 +5,13 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 
 import com.titulation.company.configuration.domain.model.Company;
+import com.titulation.company.configuration.domain.model.PasswordResponse;
 import com.titulation.company.configuration.domain.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,15 +40,18 @@ public class CompanyContoller {
         return new ResponseEntity<>(companyService.saveCompany(company), OK);
     }
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/id/{idEmpresa}/password/{password}")
-    public ResponseEntity<String> checkPassword(@PathVariable("idEmpresa") String idEmpresa, 
+    public ResponseEntity<PasswordResponse> checkPassword(@PathVariable("idEmpresa") String idEmpresa, 
             @PathVariable("password") String password) {
+        PasswordResponse passwordResponse = new PasswordResponse();
         String passwordCheck = companyService.checkPassword(idEmpresa, password);
+        passwordResponse.setPasswordResponse(passwordCheck);
         HttpStatus status = NOT_FOUND;
         if(passwordCheck.equals("pass")){
             status = OK;
         }
-        return new ResponseEntity<>(passwordCheck, status);
+        return new ResponseEntity<>(passwordResponse, status);
     }
 
 }
